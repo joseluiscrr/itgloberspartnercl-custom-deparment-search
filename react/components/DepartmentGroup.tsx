5a967b29852bfeeaaeb67ec06f91bdec51541a33
joseluiscrr <joseluiscriollojlcr@gmail.com>
2022-09-19 16:57:29 -0500

    feat: end component

---
+++ react/components/DepartmentGroup.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+type Props = {
+  departments: [Category],
+  handleSetSlug: any
+};
+
+type Category = {
+  id: string,
+  name: string,
+  slug: string
+}
+
+const DepartmentGroup = ({ departments, handleSetSlug }: Props) => {
+  console.log("Mi grupo de departamento es:", departments);
+  const onHandleSetSlug = (event: any) => {
+    return handleSetSlug(`${event.target.value}/$\{term\}&map=ft`)
+  };
+
+  const departmentOptions: any = departments.map((department: Category) => {
+    return (
+      <option value={department.slug} key={department.id}>
+        {department.name}
+      </option>
+    );
+  });
+
+  return (
+    <select defaultValue="value0" onChange={onHandleSetSlug}>
+      <option value="value0">Selecciona una opción</option>
+      {departmentOptions}
+    </select>
+  );
+};
+
+export default DepartmentGroup;
